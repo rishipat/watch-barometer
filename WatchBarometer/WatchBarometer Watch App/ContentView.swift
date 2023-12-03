86ae8eecc5fbf580087edebeeaab879cbfb2fa82
Rishi Patel <rishi@Rishis-MacBook-Pro.local>
2023-12-03 00:31:55 -0800

    initial template

---
+++ WatchBarometer/WatchBarometer Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchBarometer Watch App
+//
+//  Created by Rishi Patel on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchBarometer/WatchBarometer Watch App/WatchBarometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchBarometerApp.swift
+//  WatchBarometer Watch App
+//
+//  Created by Rishi Patel on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchBarometer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
